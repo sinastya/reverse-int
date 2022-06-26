cac6dcb51d4ee57da9a007f51f4077037d44699f
sinastya <sinastya@yandex.ru>
2022-06-26 13:02:16 +0300

    complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
+  let result = "";
+  let i = 0;
+  let n_str = Math.abs(n).toString();
+
+  while (i < n_str.length) {
+   result = `${n_str[i]}${result}`;
+   i ++;
+  }
+  return +result;
 }
